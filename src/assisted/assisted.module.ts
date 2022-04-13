b57d044c86c15dfe41d4498e575c44d7bd232484
Christian Farias <chriis_farias@hotmail.com>
2022-04-13 17:09:06 -0300

    assisted directory moved

--- src/assisted/assisted.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AssistedService } from './assisted.service';
-import { AssistedController } from './assisted.controller';
-
-@Module({
-  controllers: [AssistedController],
-  providers: [AssistedService]
-})
-export class AssistedModule {}
--- src/assisted/dto/update-assisted.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAssistedDto } from './create-assisted.dto';
-
-export class UpdateAssistedDto extends PartialType(CreateAssistedDto) {}
